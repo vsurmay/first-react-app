ce04f07031541d2e46ed334d849538f8a36ffb0d
Vitalii Surmai <vsurmaj510@gmail.com>
2023-02-28 13:20:59 +0200

    test

--- src/App.js
+++ src/App.js
@@ -3,18 +3,42 @@ import "./App.css";
 function App() {
   const name = "Vitalik!";
   const elem = <h1>Hey! {name}</h1>;
-  const user = {
-    firstName: "Vitalik",
-    lastName: "Surmay",
-  };
+  const users = [
+    {
+      firstName: "Oleg",
+      lastName: "Mant",
+    },
+    {
+      firstName: "Lofjd",
+      lastName: "Idja",
+    },
+    {
+      firstName: "Pofsd",
+      lastName: "Aqdhfdsf",
+    },
+    {
+      firstName: "Afjlsdj",
+      lastName: "Hfsdd",
+    },
+    {
+      firstName: "Vitalik",
+      lastName: "Surmay",
+    },
+    {
+      firstName: "Vitalik",
+      lastName: "Surmay",
+    },
+  ];
 
-  function test(user) {
-    return <h2>{`Hi ${user.firstName} ${user.lastName}`}</h2>;
+  function test(users) {
+    return (
+      <li>{users.map((user) => `Hey ${user.firstName} ${user.lastName}`)}</li>
+    );
   }
   return (
     <div>
       {elem}
-      {test(user)}
+      <ul>{test(users)}</ul>
     </div>
   );
 }
