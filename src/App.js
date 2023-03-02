2e305e8026d4d9e43e524fd251cf4693a863e27f
Vitalii Surmai <vsurmaj510@gmail.com>
2023-03-02 18:56:05 +0200

    aded sass

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import "./LifeCicle.js";
 import Test from "./LifeCicle";
 import React from "react";
+import Test1 from "./Components/Test/Test1";
 
 function App() {
   return (
     <div>
       Hey!
       <Test />
+      <Test1 />
     </div>
   );
 }
---
+++ src/Components/Test/Test1.js
@@ -0,0 +1,7 @@
+import "./Test.scss";
+
+function Test1() {
+  return <div className="testt">Hey</div>;
+}
+
+export default Test1;
