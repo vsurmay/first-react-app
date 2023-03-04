14eb8338e325b5052b748c88a595a327d7b5803e
Vitalii Surmai <vsurmaj510@gmail.com>
2023-03-04 11:33:36 +0200

    delete fies

--- src/Components/Test/Test1.js
+++
@@ -1,7 +0,0 @@
-import "./Test.scss";
-
-function Test1(props) {
-  return <div className="testt">Hey  {props.text}</div>;
-}
-
-export default Test1;
--- src/Components/Test2/Test2.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-class Test2 extends React.Component {
-  render() {
-    return <div>Hey {this.props.text}</div>;
-  }
-}
-
-export default Test2;
