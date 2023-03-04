14410d5c7da1d5bd2a84a06b2a81c9d4cb0e86b4
Vitalii Surmai <vsurmaj510@gmail.com>
2023-03-04 16:26:11 +0200

    ended page1

--- src/components/Recipes/Recipes.js
+++ src/components/Recipes/Recipes.js
@@ -1,13 +1,20 @@
 import Title from "../Title/Title";
+import RecipesItem from "../RecipesItem/RecipesItem";
+import data from "./data";
 import "./Recipes.scss";
 
 function Recipes() {
+  console.log(data);
   return (
     <section className="section__recipes">
       <div className="container">
         <div className="recipes">
           <Title center>Recipes</Title>
-          <div className="recipes__wrapper"></div>
+          <div className="recipes__wrapper">
+            {data.map((el) => (
+              <RecipesItem key={el.id} element={el} />
+            ))}
+          </div>
         </div>
       </div>
     </section>
---
+++ src/components/RecipesItem/RecipesItem.js
@@ -0,0 +1,26 @@
+import Title from "../Title/Title";
+import PageText from "../PageText/PageText";
+import "./RecipesItem.scss";
+
+function RecipesItem(props) {
+  console.log(props.element);
+  return (
+    <div className="recipes__item">
+      <img
+        className="recipes__item-img"
+        src={props.element.image}
+        alt="image"
+      />
+      <div className="recipes__item-content">
+        <Title className="recipes__item-title" center less>
+          {props.element.name}
+        </Title>
+        <PageText className="recipes__item-text" center>
+          {props.element.description}
+        </PageText>
+      </div>
+    </div>
+  );
+}
+
+export default RecipesItem;
