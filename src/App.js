b11c63e4c52533b1176460101893eca9a2651c6d
Vitalii Surmai <vsurmaj510@gmail.com>
2023-03-08 19:46:53 +0200

    change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import "./App.css";
+import "./App.scss";
 
 function App() {
-  return;
+  const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+  return (
+    <div className="container">
+      <h1 className="game__title">Crosses Zeros</h1>
+      <div className="game">
+        <div className="game__item"></div>
+        <div className="game__item"></div>
+        <div className="game__item"></div>
+        <div className="game__item"></div>
+        <div className="game__item"></div>
+        <div className="game__item"></div>
+        <div className="game__item"></div>
+        <div className="game__item"></div>
+        <div className="game__item"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
